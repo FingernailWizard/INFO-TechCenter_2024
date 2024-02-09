1f31778b7b9bb28b4ea5c5d6846ee23822df4a19
Maher <02James.MAher@gulllakecs.org>
2024-02-09 14:05:13 -0500

    Import Time Library-called in and stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -1,2 +1,9 @@
+
+#Import Libraries Here
+import time
+
+timeToSleep = 2
+
 print("\n\nWelcome to Info Tech center version 1.0")
+time.sleep(timeToSleep)
 print("\nInfo Tech Center Operating System Grimacing...")
