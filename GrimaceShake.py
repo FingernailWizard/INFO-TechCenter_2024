343110ed3b1961d8b6f53160da68660683c552a8
Maher <02James.MAher@gulllakecs.org>
2024-02-20 13:24:38 -0500

    Added Gas Level alert

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -9,4 +9,20 @@ def gasLevelGauge():
     currentGasLevel = random.choice(gasLevelList)
     return currentGasLevel
 
+#Funtion that lists gas stations, randomly choosing one and returning its value
+
+def listOfGasStations():
+    gasStations = ["Shell", "Speedway", "Kum And Go", "Circle K", "Moble", "Costco", "meijor", "7Eleven"]
+    gasStationsNearby = random.choice(gasStations)
+    return gasStationsNearby
+
+def gasLevelAlert():
+    milesToGasStationLow = random.uniform(1,25)
+    milesToGasStationQuarterTank = random.uniform(25.1,50)
+
+    print(milesToGasStationLow)
+    print(milesToGasStationQuarterTank)
+
+
 print(gasLevelGauge())
+print(listOfGasStations())
