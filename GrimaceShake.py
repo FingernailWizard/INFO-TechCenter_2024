2a3d7aa630865f5c32413008f8da041944499a4e
Maher <02James.MAher@gulllakecs.org>
2024-02-20 14:11:12 -0500

    Add if Statement for Empty-Stable

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -3,6 +3,7 @@ print("Gasoline Branch\n\n")
 
 #import libraries here
 import random
+from time import sleep
 
 def gasLevelGauge():
     gasLevelList = ["Empty", "Low", "Quarter Tank", "Half Tank","Three Quarter Tank", "Full Tank"]
@@ -19,8 +20,12 @@ def listOfGasStations():
 def gasLevelAlert():
     milesToGasStationLow = round(random.uniform(1,25),1)
     milesToGasStationQuarterTank = round(random.uniform(25.1,50),1)
+    gasLevelIndicator = gasLevelGauge()
+    if gasLevelIndicator == "Empty":
+        print("***WARNING - YOU ARE ON EMPTY***")
+        sleep(1.25)
+        print("Calling Triple AAA")
 
-    print(milesToGasStationLow)
-    print(milesToGasStationQuarterTank)
+
 
 gasLevelAlert()
