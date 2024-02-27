2f6253ec4de7a954e16bb8b64a578c846ceb8ee8
Maher <02James.MAher@gulllakecs.org>
2024-02-27 13:39:53 -0500

    UPDATED  weather function code

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -16,16 +16,32 @@ weatherAlert = weather()
 
 def vehicleRepsonseSystem():
     if weatherAlert == "Snowing":
-        print("\nNational Weather Service has updated our alarm by 30 minutes because of the the forcast of",weatherAlert,
-              "weather conditions.")
-        print("VRS has been engaged only allowing you to drive 50mph")
+        print("\nNational Weather Service Has Updated Our Alarm By 30 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 50mph")
     elif weatherAlert == "Blizzard":
-        print("\nNational Weather Service has updated our alarm by 45 minutes because of the the forcast of",weatherAlert,
-              "weather conditions.")
-        print("VRS has been engaged only allowing you to drive 40mph")
-    elif weatherAlert == "raining":
-        print("\nNational Weather Service has updated our alarm by 10 minutes because of the the forcast of",weatherAlert,
-              "weather conditions.")
-        print("VRS has been engaged only allowing you to drive 60mph")
+        print("\nNational Weather Service Has Updated Our Alarm By 45 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 40mph")
+    elif weatherAlert == "Raining":
+        print("\nNational Weather Service Has Updated Our Alarm By 10 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 60mph")
+    elif weatherAlert == "Foggy":
+        print("\nNational Weather Service Has Updated Our Alarm By 10 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 60mph")
+    elif weatherAlert == "Windy":
+        print("\nNational Weather Service Has Updated Our Alarm By 5 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 80mph")
+    elif weatherAlert == "Icy":
+        print("\nNational Weather Service Has Updated Our Alarm By 60 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive 30mph")
+    else:
+        print("\nNational Weather Service Has Updated Our Alarm By 0 Minutes Because Of The Forcast Of",weatherAlert,
+              "Weather Conditions.")
+        print("VRS Has Been Engaged Only Allowing You To Drive MAXmph")
 
 vehicleRepsonseSystem()
