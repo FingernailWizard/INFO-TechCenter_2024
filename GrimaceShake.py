35eb29b11526b04ab48efb380c010c377ac621cb
Maher <02James.MAher@gulllakecs.org>
2024-02-27 13:44:55 -0500

    Added Sleep Timer in between weather funtions

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -18,30 +18,37 @@ def vehicleRepsonseSystem():
     if weatherAlert == "Snowing":
         print("\nNational Weather Service Has Updated Our Alarm By 30 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 50mph")
     elif weatherAlert == "Blizzard":
         print("\nNational Weather Service Has Updated Our Alarm By 45 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 40mph")
     elif weatherAlert == "Raining":
         print("\nNational Weather Service Has Updated Our Alarm By 10 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 60mph")
     elif weatherAlert == "Foggy":
         print("\nNational Weather Service Has Updated Our Alarm By 10 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 60mph")
     elif weatherAlert == "Windy":
         print("\nNational Weather Service Has Updated Our Alarm By 5 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 80mph")
     elif weatherAlert == "Icy":
         print("\nNational Weather Service Has Updated Our Alarm By 60 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive 30mph")
     else:
         print("\nNational Weather Service Has Updated Our Alarm By 0 Minutes Because Of The Forcast Of",weatherAlert,
               "Weather Conditions.")
+        sleep(1.5)
         print("VRS Has Been Engaged Only Allowing You To Drive MAXmph")
 
 vehicleRepsonseSystem()
